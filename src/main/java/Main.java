cfb84c8ae1ef1e4f2c2ed25cd48d4cb7e694da7b
Mark Medum Bundgaard <mmedum@gmail.com>
2015-10-08 22:03:46 +0200

    changed hello message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        new WebServer().configure(routes -> routes.get("/", "Hello World")).start();
+        new WebServer().configure(routes -> routes.get("/", "Hello SpareTimeTeaching")).start();
 
     }
 }
